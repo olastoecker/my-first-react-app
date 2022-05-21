64465d3b8e62c64817c3bf8c7723ac347cbffd9f
Alexandra Stoecker <ola.stocker@gmail.com>
2022-05-21 19:20:03 +0200

    test commit

---
+++ src/components/Column/Column.js
@@ -0,0 +1,12 @@
+import styles from './Column.module.scss';
+
+const Column = props => {
+    return (
+        <article className={styles.column}>
+            <h2 className={styles.title}>{props.title}</h2>
+            <span className={styles.icon + ' fa fa-' + props.icon} />
+        </article>
+    );
+};
+
+export default Column;
